b6f862cd82f6a7f48f494e3a6a3085d4e18d2a22
leshary <lesha-ry@yandex.ru>
2017-01-09 21:49:48 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+using namespace std;
+
+ void incredible_sort (int n, int k)
+ {
+    int* a = new int [k];
+    int b;//число с консоли
+    for (int i = 0; i < k; i ++)
+        a[i]=0;
+    for (int i = 0; i < n;i++){
+        cin>>b;
+        a[b-1]++;
+        }
+    for (int i = k-1; i > -1; i--)
+        for (int j = 0; j < a[i]; j++)
+            cout<<i+1<<' ';
+ }
+
+int main()
+{
+    int n;
+    int k;
+    cin>>n>>k;
+    incredible_sort ( n , k );
+    return 0;
+}
